c86d3687112b1e18ee0e01fddb2f8ebfb57bb56f
emiliyamon <emiliyam@campus.technion.ac.il>
2023-06-01 17:01:12 +0300

    0106 1700 stated classes Date and DateTime

--- src/Date.java
+++ src/Date.java
@@ -1,12 +1,29 @@
 public class Date {
-    int day;
-    int month;
-    int year;
+    int day; // default = 1
+    int month; // default = 1
+    int year; // default = 0
 
     public Date(int day, int month, int year){
         this.day = day;
         this.month = month;
         this.year = year;
+    }
+
+    public int hashCode() {
+        int hashCode;
+        return hashCode;
+    }
+
+
+    @Override
+    public boolean equals(Date date) {
+        if (this = date) {
+            return true;
+        }
+        if () {
+            return false;
+        }
+
     }
 
 }
