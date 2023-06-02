946462df3aa52fe0aefc1443a355a98317983246
emiliyamon <emiliyam@campus.technion.ac.il>
2023-06-02 21:17:41 +0300

    0206 2117 updates in Date

--- src/Date.java
+++ src/Date.java
@@ -1,22 +1,72 @@
 public class Date {
-    int day; // default = 1
-    int month; // default = 1
-    int year; // default = 0
+    private static final int DAY_MIN_VALUE = 1;
+    private static final int MONTH_MIN_VALUE = 1;
+    private static final int YEAR_MIN_VALUE = -3999;
+    private static final int DAY_MAX_VALUE = 31;
+    private static final int MONTH_MAX_VALUE = 12;
+    private static final int YEAR_MAX_VALUE = 3999;
+    private static final int DAY_DEFAULT_VALUE = 1;
+    private static final int MONTH_DEFAULT_VALUE = 1;
+    private static final int YEAR_DEFAULT_VALUE = 0;
 
-    public Date(int day, int month, int year){
-        this.day = day;
-        this.month = month;
-        this.year = year;
+
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int day, int month, int year) {
+        setDay(day);
+        setMonth(month);
+        setYea(year);
     }
 
 
-    public Date(){
-        this.day = 1;
-        this.month = 1;
-        this.year = 0;
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
     }
 
 
+    public boolean inRange(int num, int lowerBound, int upperBound) {
+        return (num >= lowerBound && num <= upperBound);
+    }
+
+
+    public void setMonth(int month) {
+        if (!inRange(month, MONTH_MIN_VALUE, MONTH_MAX_VALUE)) {
+            this.month = MONTH_DEFAULT_VALUE;
+        } else {
+            this.month = month;
+        }
+    }
+
+
+    public void setYear(int year) {
+        if (!inRange(year, YEAR_MIN_VALUE, YEAR_MAX_VALUE)) {
+            this.year = YEAR_DEFAULT_VALUE;
+        } else {
+            this.year = year;
+        }
+    }
+
+
+    public void setDay(int day) {
+        if (!inRange(day, DAY_MIN_VALUE, DAY_MAX_VALUE)) {
+            this.day = DAY_DEFAULT_VALUE;
+        } else {
+            this.day = day;
+        }
+    }
+
+
+    @Override
     public int hashCode() {
         int hashCode;
         return hashCode;
