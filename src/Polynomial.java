5a03dd7edf3f9ff0a9cac18d0e4211c957688408
emiliyamon <emiliyam@campus.technion.ac.il>
2023-06-07 05:58:15 +0300

    0706 0558 Polynomial

---
+++ src/Polynomial.java
@@ -0,0 +1,55 @@
+public class Polynomial extends Function {
+    private static double[] coefficients;
+
+    public Polynomial(double... coefficientsSet) {
+        int length = coefficientsSet.length;
+        double[] coefficients = new double[length];
+        for (int i = 0; i < length; i++) {
+            coefficients[i] = coefficientsSet[i];
+        }
+    }
+
+
+    @Override
+    public double valueAt(double x) {
+        double value = 0.0;
+        for (int i = 0; i < coefficients.length; i++) {
+            value += coefficients[i] * Math.pow(x, i);
+        }
+        return value;
+    }
+
+
+    @Override
+    public String toString() {
+        String[] terms = new String[coefficients.length];
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < coefficients.length; i++) {
+            if (coefficients[i] == 0.0) {
+                continue;
+            } else if (coefficients[i] == 1.0) {
+                terms[i] = "x^" + i;
+            } else if (coefficients[i] == -1.0) {
+                terms[i] = "-x^" + i;
+            } else if (coefficients[i] == (int) coefficients[i]) {
+                terms[i] = (int) coefficients[i] + "x^" + i;
+            }
+            if (coefficients[i] < 0) {
+                sb.append(coefficients[i]);
+                sb.append("+");
+            } else {
+                sb.append(coefficients[i]);
+                sb.append("-");
+            }
+        }
+        return sb.toString();
+    }
+
+
+    @Override
+    public Function derivative() {
+        return new Constant(0);
+    }
+
+}
